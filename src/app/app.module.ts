732bf8b1b50aa45c43c216239b087a9ef5b7057a
Your Name <your-email@example.com>
2025-03-01 21:39:27 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // Import this
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [],
   imports: [
     HttpClientModule,
     BrowserModule,
+    CommonModule,
+    FormsModule,
     AppRoutingModule, // Add this here
   ],
   providers: [],
