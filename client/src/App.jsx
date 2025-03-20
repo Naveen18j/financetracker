ef61ace9772cc5dae20a79c634a1de3a9d44ddb7
Naveen18j <jnaveenk2004@gmail.com>
2025-03-20 12:11:39 +0530

    chnages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Report from "./pages/Report";
+import React from 'react';
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
