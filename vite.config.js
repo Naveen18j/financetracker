0bccef39d925dfe4e7fc2e2c11be2713a432a5e0
Naveen18j <jnaveenk2004@gmail.com>
2025-03-20 12:16:39 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src', 
-})
+  root: './src', // Adjust this path if your index.html is elsewhere
+  build: {
+    outDir: '../dist', // output directory for the build
+  },
+});
